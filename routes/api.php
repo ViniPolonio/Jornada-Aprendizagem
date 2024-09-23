5e682c56869f385554754664d4d0164173067b86
ViniPolonio <poloniober@gmail.com>
2024-09-23 15:22:20 -0300

    atualização rota Login

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/login', [App\Http\Controllers\UserController::class, 'login'])->name('login');
+Route::post('/login', [App\Http\Controllers\UserController::class, 'login']);
+Route::post('/logout', [App\Http\Controllers\UserController::class, 'logout'])->name('logout');
